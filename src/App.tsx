1ed7bfb00794c0acdea0ee2c6323519ccf9cbeb2
mir <djalilovmir@gmail.com>
2020-09-24 02:11:21 +0300

    material-ui-is-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(["kdjf", "ksdfj"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world🚀</h1>
+      <FormControl>
+        <InputLabel>Write a todo</InputLabel>
+        <Input
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+      </FormControl>
+
+      <Button
+        disabled={!input}
+        variant="contained"
+        onClick={addTodo}
+        color="primary"
+      >
+        Add todo
+      </Button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
